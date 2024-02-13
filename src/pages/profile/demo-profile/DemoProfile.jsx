d57811eef2cd911d001a89cbff3e9d20e307f0fa
vismodev <vismodeb2000@gmail.com>
2024-02-13 15:14:19 +0600

    demo profile social media icon add

--- src/pages/profile/demo-profile/DemoProfile.jsx
+++ src/pages/profile/demo-profile/DemoProfile.jsx
@@ -3,6 +3,9 @@ import './DemoProfile.css';
 import { FaCamera } from "react-icons/fa";
 import coverImg from "../../../assets/images/banner-1.webp";
 import profileImg from "../../../assets/images/vismo.jpg";
+import { FaSquareFacebook, FaSquareXTwitter, FaTiktok } from "react-icons/fa6";
+import { IoLogoLinkedin } from "react-icons/io5";
+import { FaInstagram, FaSnapchat, FaGlobe } from "react-icons/fa";
 
 const DemoProfile = () => {
   return (
@@ -21,7 +24,7 @@ const DemoProfile = () => {
               </div>
             </div>
             <div className="col-lg-3 mt-4">
-              <div className="text_contain">
+              <div className="text_contain text-gray-600">
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore quaerat tempora eos! Voluptate alias atque est nulla minus iure, corrupti distinctio ipsam maxime? Dignissimos exercitationem unde voluptate, earum natus temporibus!</p>
               </div>
             </div>
@@ -30,7 +33,7 @@ const DemoProfile = () => {
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat est animi delectus quos distinctio, temporibus odio a laboriosam id magnam? Ratione quas sunt tenetur voluptate beatae? A quod nobis aut.</p>
               </div>
               <div className="text_contain">
-                <h2 className='text_ph'>About Me</h2>
+                <h2 className='text_ph text-gray-600'>About Me</h2>
                 <div className="row mt-2">
                   <div className="col-md-6 table-responsive-sm">
                     <table className="table">
@@ -85,10 +88,10 @@ const DemoProfile = () => {
               <div className="text_contain">
                 <div className="row">
                   <div className="col-md-6">
-                    <h2 className='text_ph'>Job Experience</h2>
+                    <h2 className='text_ph text-gray-600'>Job Experience</h2>
                     <div className="row mt-2">
                       <div className="col-md-12">
-                        <div class="job_experience contact_info mt-0">
+                        <div className="job_experience contact_info mt-0 text-gray-600">
                           <ul>
                             <li>Company : SunWings Tours and Travels</li>
                             <li>Designation : Expert Web Designer</li>
@@ -99,7 +102,7 @@ const DemoProfile = () => {
                     </div>
                     <div className="row mt-3">
                       <div className="col-md-12">
-                        <div class="job_experience contact_info mt-0">
+                        <div className="job_experience contact_info mt-0 text-gray-600">
                           <ul>
                             <li>Company : SunWings Tours and Travels</li>
                             <li>Designation : Expert Web Designer</li>
@@ -110,10 +113,10 @@ const DemoProfile = () => {
                     </div>
                   </div>
                   <div className="col-md-6">
-                    <h2 className='text_ph'>Education</h2>
+                    <h2 className='text_ph text-gray-600'>Education</h2>
                     <div className="row mt-2">
                       <div className="col-md-12">
-                        <div class="job_experience contact_info mt-0">
+                        <div className="job_experience contact_info mt-0 text-gray-600">
                           <ul>
                             <li>Certificate : Secoundary School Certificate</li>
                             <li>Institution : EUB</li>
@@ -124,11 +127,189 @@ const DemoProfile = () => {
                     </div>
                     <div className="row mt-3">
                       <div className="col-md-12">
-                        <div class="job_experience contact_info mt-0">
+                        <div className="job_experience contact_info mt-0 text-gray-600">
                           <ul>
                             <li>Certificate : Secoundary School Certificate</li>
                             <li>Institution : EUB</li>
                             <li>Passing Year : Jan 2023 - Jul 2024</li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="text_contain">
+                <div className="row">
+                  <div className="col-md-6">
+                    <h2 className='text_ph text-gray-600'>Social Media</h2>
+                    <div className="row mt-2">
+                      <div className="col-md-12">
+                        <div className="job_experience contact_info mt-0">
+                          <ul className=''>
+                            <li>
+                              <Link to="#">
+                                <div className="flex items-center gap-2 text-base text-gray-600">
+                                  <FaSquareFacebook className='text-3xl text-blue-600' />
+                                  <span>https://www.facebook.com</span>
+                                </div>
+                              </Link>
+                            </li>
+                            <li>
+                              <Link to="#">
+                                <div className="flex items-center gap-2 text-base text-gray-600">
+                                  <FaSquareFacebook className='text-3xl text-blue-600' />
+                                  <span>https://www.facebook.com</span>
+                                </div>
+                              </Link>
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="row mt-3">
+                      <div className="col-md-12">
+                        <div className="job_experience contact_info mt-0">
+                          <ul>
+                            <li>
+                              <Link to="#">
+                                <div className="flex items-center gap-2 text-base text-gray-600">
+                                  <FaSquareXTwitter className='text-3xl text-black' />
+                                  <span>https://www.twitter.com</span>
+                                </div>
+                              </Link>
+                            </li>
+                            <li>
+                              <Link to="#">
+                                <div className="flex items-center gap-2 text-base text-gray-600">
+                                  <FaSquareXTwitter className='text-3xl text-black' />
+                                  <span>https://www.twitter.com</span>
+                                </div>
+                              </Link>
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="row mt-3">
+                      <div className="col-md-12">
+                        <div className="job_experience contact_info mt-0">
+                          <ul>
+                            <li>
+                              <Link to="#">
+                                <div className="flex items-center gap-2 text-base text-gray-600">
+                                  <IoLogoLinkedin className='text-3xl text-blue-700' />
+                                  <span>https://www.linkedin.com</span>
+                                </div>
+                              </Link>
+                            </li>
+                            <li>
+                              <Link to="#">
+                                <div className="flex items-center gap-2 text-base text-gray-600">
+                                  <IoLogoLinkedin className='text-3xl text-blue-700' />
+                                  <span>https://www.linkedin.com</span>
+                                </div>
+                              </Link>
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="row mt-3">
+                      <div className="col-md-12">
+                        <div className="job_experience contact_info mt-0">
+                          <ul>
+                            <li>
+                              <Link to="#">
+                                <div className="flex items-center gap-2 text-base text-gray-600">
+                                  <FaInstagram className='text-2xl gradient-instagram' />
+                                  <span>https://www.instagram.com</span>
+                                </div>
+                              </Link>
+                            </li>
+                            <li>
+                              <Link to="#">
+                                <div className="flex items-center gap-2 text-base mt-2 text-gray-600">
+                                  <FaInstagram className='text-2xl gradient-instagram' />
+                                  <span>https://www.instagram.com</span>
+                                </div>
+                              </Link>
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <h2 className='text_ph'></h2>
+                    <div className="row mt-3">
+                      <div className="col-md-12">
+                        <div className="job_experience contact_info mt-0">
+                          <ul>
+                            <li>
+                              <Link to="#">
+                                <div className="flex items-center gap-2 text-base text-gray-600">
+                                  <FaTiktok className='text-2xl text-black' />
+                                  <span>https://www.Tiktok.com</span>
+                                </div>
+                              </Link>
+                            </li>
+                            <li>
+                              <Link to="#">
+                                <div className="flex items-center gap-2 text-base mt-2 text-gray-600">
+                                  <FaTiktok className='text-2xl text-black' />
+                                  <span>https://www.Tiktok.com</span>
+                                </div>
+                              </Link>
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="row mt-3">
+                      <div className="col-md-12">
+                        <div className="job_experience contact_info mt-0">
+                          <ul>
+                            <li>
+                              <Link to="#">
+                                <div className="flex items-center gap-2 text-base text-gray-600">
+                                  <FaSnapchat className='text-2xl text-yellow-400' />
+                                  <span>https://www.Tiktok.com</span>
+                                </div>
+                              </Link>
+                            </li>
+                            <li>
+                              <Link to="#">
+                                <div className="flex items-center gap-2 text-base mt-2 text-gray-600">
+                                  <FaSnapchat className='text-2xl text-yellow-400' />
+                                  <span>https://www.Tiktok.com</span>
+                                </div>
+                              </Link>
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="row mt-3">
+                      <div className="col-md-12">
+                        <div className="job_experience contact_info mt-0">
+                          <ul>
+                            <li>
+                              <Link to="#">
+                                <div className="flex items-center gap-2 text-base text-gray-600">
+                                  <FaGlobe className='text-2xl text-black' />
+                                  <span>https://www.website.com</span>
+                                </div>
+                              </Link>
+                            </li>
+                            <li>
+                              <Link to="#">
+                                <div className="flex items-center gap-2 text-base mt-2 text-gray-600">
+                                  <FaGlobe className='text-2xl text-black' />
+                                  <span>https://www.website.com</span>
+                                </div>
+                              </Link>
+                            </li>
                           </ul>
                         </div>
                       </div>
